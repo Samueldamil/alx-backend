4d88c744a0f51802d2714f886063f6732105f013
Samueldamil <damilareedun@gmail.com>
2023-04-25 18:37:03 +0000

    file

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python3
 """
-Module 0-app
+A Basic flask application
 """
-from flask import Flask, render_template, request
+from flask import Flask
+from flask import request
+from flask import render_template
 from flask_babel import Babel
 
 
 class Config(object):
-    """i18n configuration"""
+    """
+    Application configuration class
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# Instantiate the application object
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Wrap the application with Babel
 babel = Babel(app)
 
 
-@app.route("/", strict_slashes=False)
-def index():
-    """didplays a basic hello world message"""
-    return render_template("3-index.html")
-
-
 @babel.localeselector
-def get_locale():
-    """Gets best fmatch locale according to request"""
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="5000")
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
